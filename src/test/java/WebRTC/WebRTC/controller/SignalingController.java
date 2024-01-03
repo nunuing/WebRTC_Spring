6c3c710b2baba1304e2860640785344d081e83dc
eycho <chw3872@gmail.com>
2024-01-03 17:27:50 +0900

    changes

--- src/test/java/WebRTC/WebRTC/controller/SignalingController.java
+++ src/test/java/WebRTC/WebRTC/controller/SignalingController.java
@@ -1,8 +1,9 @@
 package WebRTC.WebRTC.controller;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@Slf4J
 public class SignalingController {
 }
